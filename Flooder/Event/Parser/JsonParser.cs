30a5ca5a888e063586fbee27d2a1ba66a245b742
yusuke yoda <y.yoda@gloops.com>
2015-04-27 12:19:18 +0900

    add json parsser

---
+++ Flooder/Event/Parser/JsonParser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Flooder.Event.Parser
+{
+    public class JsonParser : IPayloadParser
+    {
+        public Dictionary<string, object> Parse(string source)
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, object>>(source);
+        }
+
+        public Dictionary<string, object>[] MultipleParse(string source)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
