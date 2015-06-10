2c8c76e9c7c686cbfe101480172c618ca6da6088
yusuke yoda <y.yoda@gloops.com>
2015-06-10 17:05:03 +0900

    Jsonのパースエラーが調査できるようにロギング

--- Flooder/Event/Parser/JsonParser.cs
+++ Flooder/Event/Parser/JsonParser.cs
@@ -1,14 +1,25 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
 using System.Collections.Generic;
-using Newtonsoft.Json;
 
 namespace Flooder.Event.Parser
 {
     public class JsonParser : IPayloadParser
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         public Dictionary<string, object> Parse(string source)
         {
-            return JsonConvert.DeserializeObject<Dictionary<string, object>>(source);
+            try
+            {
+                return JsonConvert.DeserializeObject<Dictionary<string, object>>(source);
+            }
+            catch (Exception ex)
+            {
+                Logger.ErrorException(string.Format("JsonParserのデシリアライズが失敗しました. source:{0}", source), ex);
+                throw;
+            }
         }
 
         public Dictionary<string, object>[] MultipleParse(string source)
