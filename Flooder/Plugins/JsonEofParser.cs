345875cea4b914d0e6905934138acd3a9566595f
yusuke yoda <y.yoda@gloops.com>
2015-08-12 22:50:14 +0900

    add JsonEofParser

---
+++ Flooder/Plugins/JsonEofParser.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Flooder.Events;
+
+namespace Flooder.Plugins
+{
+    public class JsonEofParser : IMultipleDictionaryParser
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        public Dictionary<string, object>[] Parse(string source)
+        {
+            var r = source.Split(new[] { "EOF" }, StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => !string.IsNullOrEmpty(x))
+                .Select(x =>
+                {
+                    try
+                    {
+                        return JsonConvert.DeserializeObject<Dictionary<string, object>>(x.Replace("\\", "\\\\"));
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.TraceException(string.Format("[SKIP] Json parse error. source:{0}", source), ex);
+                        return null;
+                    }
+                })
+                .Where(x => x != null)
+                .ToArray();
+
+            return r;
+        }
+    }
+}
